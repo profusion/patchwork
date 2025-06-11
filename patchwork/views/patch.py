b118494832e92b709ef0a5bf0464e7cbcb7225b1
DATVenancio <daniel.venancio@profusion.mobi>
2025-06-11 16:40:56 -0300

    views: grant maintainer privileges to superusers

--- patchwork/views/patch.py
+++ patchwork/views/patch.py
@@ -66,6 +66,7 @@ def patch_detail(request, project_id, msgid):
     is_maintainer = (
         request.user.is_authenticated
         and project in request.user.profile.maintainer_projects.all()
+        or request.user.is_superuser
     )
 
     form = None
