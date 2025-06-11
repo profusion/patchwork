9fbcd31db87369ec489395c93c332cd1e4d45880
DATVenancio <daniel.venancio@profusion.mobi>
2025-06-11 16:48:57 -0300

    views: fix include in attention set and review interest count

--- patchwork/views/patch.py
+++ patchwork/views/patch.py
@@ -90,9 +90,7 @@ def patch_detail(request, project_id, msgid):
         elif action in ['add-interest', 'remove-interest']:
             if request.user.is_authenticated:
                 if action == 'add-interest':
-                    PatchAttentionSet.objects.get_or_create(
-                        patch=patch, user=request.user
-                    )
+                    PatchAttentionSet.objects.upsert(patch, [request.user.id])
                     message = (
                         'You have declared interest in reviewing this patch'
                     )
